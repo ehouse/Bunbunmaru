6e84012a3582f94d49459c3957d4d6c183e72f6a
Ethan House <ehouse@csh.rit.edu>
2013-04-27 22:25:45 -0400

    Added Game Loop

--- Bunbunmaru.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import main
-
-if __name__ == '__main__':
-    main.start()
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import engine.enemy
+#!/usr/bin/python
+#system imports
+import os,sys
+import pygame
+from pygame.locals import *
+#local imports
+import engine
+import resources
 
-def start():
-    pass
+class GameLoop:
+    def __init__(self,width=640,height=480):
+        self.game = pygame
+        self.game.init()
+        self.width = width
+        self.height = height
+        self.screen = self.game.display.set_mode((self.width,self.height))
+        self.game.display.set_caption('Bunbunmaru Gamejam 2013')
+
+    def start(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+
+if __name__ == "__main__":
+    bun = GameLoop()
+    bun.start()
